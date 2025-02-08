e6e4321afd8b3a7a0b5326e7f4521600057d8cd0
Neil Forrester <neil@nforrester.net>
2025-02-07 20:51:56 -0800

    Update celestron links in fetch.sh

--- tle/fetch.sh
+++ tle/fetch.sh
@@ -10,8 +10,8 @@ rm -f geo.txt
 rm -f gps-ops.txt
 rm -f stations.txt
 
-wget https://celestrak.com/NORAD/elements/visual.txt
-wget https://celestrak.com/NORAD/elements/starlink.txt
-wget https://celestrak.com/NORAD/elements/geo.txt
-wget https://celestrak.com/NORAD/elements/gps-ops.txt
-wget https://celestrak.com/NORAD/elements/stations.txt
+wget -O visual.txt   https://celestrak.org/NORAD/elements/gp.php?GROUP=visual&FORMAT=tle
+wget -O starlink.txt https://celestrak.org/NORAD/elements/gp.php?GROUP=starlink&FORMAT=tle
+wget -O geo.txt      https://celestrak.org/NORAD/elements/gp.php?GROUP=geo&FORMAT=tle
+wget -O gps-ops.txt  https://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle
+wget -O stations.txt https://celestrak.org/NORAD/elements/gp.php?GROUP=stations&FORMAT=tle
