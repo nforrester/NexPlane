3afc8ffe6d7f91c38be17660d42085838dedb3f0
Neil Forrester <neil@nforrester.net>
2025-02-23 16:12:33 -0800

    Give skywatcher_wifi_hootl.py a default port

--- skywatcher_wifi_hootl.py
+++ skywatcher_wifi_hootl.py
@@ -7,6 +7,10 @@ import sys
 import skywatcher
 
 def main() -> None:
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    else:
+        port = 11880
     server = skywatcher.SkyWatcherUdpServerHootl(int(sys.argv[1]))
     server.run()
 
