8a4a8c7c67deb5305706f141ced9b0b06af8b7a0
Neil Forrester <neil@nforrester.net>
2025-02-25 21:59:55 -0800

    Remove extraneous loop

--- nexplane.py
+++ nexplane.py
@@ -111,153 +111,151 @@ def main() -> None:
 
     gui_iface = gui.Gui(args.bw, args.white_bg, layers)
 
-    while True:
-        try:
-            # Telescope control interface.
-            telescope = align.setup_telescope_interface(args, serial_iface)
+    try:
+        # Telescope control interface.
+        telescope = align.setup_telescope_interface(args, serial_iface)
 
-            # Tracking controller, sends commands to the telescope.
-            target_tracker = tracker.Tracker(telescope, kp, ki, kd, (args.mount_mode == 'altaz'))
+        # Tracking controller, sends commands to the telescope.
+        target_tracker = tracker.Tracker(telescope, kp, ki, kd, (args.mount_mode == 'altaz'))
 
-            # Keeps track of how many times we've updated the controller gains.
-            last_gain_changes = 0
+        # Keeps track of how many times we've updated the controller gains.
+        last_gain_changes = 0
 
-            # Alignment
-            if args.landmark:
-                assert not args.alignment, '--landmark and --alignment are mutually exclusive.'
-                if args.mount_mode == 'altaz':
-                    azm_cal, alt_cal = align.azm_alt_cal(telescope, args.landmark, config_data, observatory_location)
-                else:
-                    assert args.mount_mode == 'eq'
-                    ra_cal, dec_cal = align.ra_dec_cal(telescope, args.landmark, config_data, observatory_location)
-            elif args.alignment:
-                with open(args.alignment) as f:
-                    alignment_data = yaml.load(f.read(), yaml.Loader)
-                if alignment_data['mount_mode'] != args.mount_mode:
-                    raise Exception('--mount-mode passed to align.py and nexplane.py must match. ' +
-                                    alignment_data['mount_mode'] + ' != ' + args.mount_mode)
-                if args.mount_mode == 'altaz':
-                    azm_cal = alignment_data['calibration']['azm']
-                    alt_cal = alignment_data['calibration']['alt']
-                else:
-                    assert args.mount_mode == 'eq'
-                    ra_cal = alignment_data['calibration']['ra']
-                    dec_cal = alignment_data['calibration']['dec']
+        # Alignment
+        if args.landmark:
+            assert not args.alignment, '--landmark and --alignment are mutually exclusive.'
+            if args.mount_mode == 'altaz':
+                azm_cal, alt_cal = align.azm_alt_cal(telescope, args.landmark, config_data, observatory_location)
             else:
-                # We'll trust that the telescope has been aligned using one of the built in methods.
+                assert args.mount_mode == 'eq'
+                ra_cal, dec_cal = align.ra_dec_cal(telescope, args.landmark, config_data, observatory_location)
+        elif args.alignment:
+            with open(args.alignment) as f:
+                alignment_data = yaml.load(f.read(), yaml.Loader)
+            if alignment_data['mount_mode'] != args.mount_mode:
+                raise Exception('--mount-mode passed to align.py and nexplane.py must match. ' +
+                                alignment_data['mount_mode'] + ' != ' + args.mount_mode)
+            if args.mount_mode == 'altaz':
+                azm_cal = alignment_data['calibration']['azm']
+                alt_cal = alignment_data['calibration']['alt']
+            else:
+                assert args.mount_mode == 'eq'
+                ra_cal = alignment_data['calibration']['ra']
+                dec_cal = alignment_data['calibration']['dec']
+        else:
+            # We'll trust that the telescope has been aligned using one of the built in methods.
+            if args.mount_mode == 'altaz':
+                azm_cal = 0.0
+                alt_cal = 0.0
+            else:
+                assert args.mount_mode == 'eq'
+                ra_cal = 0.0
+                dec_cal = 0.0
+
+        # Main loop
+        warn_comm_failure = False
+        while True:
+            try:
+                gui_comm_warning.update_comm_failure(warn_comm_failure)
+
+                time.sleep(0.05)
+
+                # Get current status of airplanes.
+                planes = sbs1_receiver.get_planes()
+
+                # Get current telescope position.
                 if args.mount_mode == 'altaz':
-                    azm_cal = 0.0
-                    alt_cal = 0.0
+                    scope_azm_raw, scope_alt_raw = telescope.get_azm_alt()
+                    scope_azm_alt = (util.wrap_rad(scope_azm_raw + azm_cal, 0), util.wrap_rad(scope_alt_raw + alt_cal, 0))
                 else:
-                    assert args.mount_mode == 'eq'
-                    ra_cal = 0.0
-                    dec_cal = 0.0
+                    scope_ra_raw, scope_dec_raw = telescope.get_ra_dec()
+                    scope_ra = util.wrap_rad(scope_ra_raw + ra_cal, -math.pi)
+                    scope_dec = util.wrap_rad(scope_dec_raw + dec_cal, -math.pi)
+                    scope_alt, scope_azm = util.radec_to_altaz(scope_ra, scope_dec, observatory_location, util.get_current_time())
+                    scope_azm_alt = (scope_azm, scope_alt)
 
-            # Main loop
-            warn_comm_failure = False
-            while True:
-                try:
-                    gui_comm_warning.update_comm_failure(warn_comm_failure)
+                # Send new data to the to GUI so it can update the drawing.
+                gui_sun_moon.update_positions(sun_azm_alt=unwrap(sun.az_el()),
+                                              moon_azm_alt=unwrap(moon.az_el()))
+                gui_telescope.update_telescope_location(scope_azm_alt)
+                gui_airplanes.update_planes(planes)
 
-                    time.sleep(0.05)
+                # Receive new user inputs from the GUI.
+                tracked_plane_hex_id = gui_airplanes.get_tracked_plane()
+                az_offset, el_offset = gui_offset_reader.get_offsets()
+                kp, ki, kd, gain_changes = gui_gain_reader.get_gains()
 
-                    # Get current status of airplanes.
-                    planes = sbs1_receiver.get_planes()
+                # If the user requested a change to the controller gains, update them accordingly.
+                if last_gain_changes != gain_changes:
+                    print('kp =', kp, 'ki =', ki, 'kd =', kd)
+                    target_tracker.set_gains(kp, ki, kd)
+                last_gain_changes = gain_changes
 
-                    # Get current telescope position.
+                if util.azm_alt_ang_dist(scope_azm_alt, unwrap(sun.az_el())) < 20/180*math.pi:
+                    # We've strayed into the keep out circle around the Sun! Emergency Stop!
+                    # The user can fix this with the hand controller.
+                    target_tracker.stop()
+                    gui_airplanes.stop_tracking()
+                    gui_offset_reader.reset_offsets()
+                elif tracked_plane_hex_id is None:
+                    # There is no airplane to track, so stop the tracker.
+                    target_tracker.stop()
+                elif tracked_plane_hex_id in planes:
+                    # Extrapolate from the last known position and velocity of the plane to estimate the current position.
+                    tracked_plane = planes[tracked_plane_hex_id].extrapolate(time.monotonic_ns())
+
+                    # Inform the target tracker of the target position and the current position of the telescope.
                     if args.mount_mode == 'altaz':
-                        scope_azm_raw, scope_alt_raw = telescope.get_azm_alt()
-                        scope_azm_alt = (util.wrap_rad(scope_azm_raw + azm_cal, 0), util.wrap_rad(scope_alt_raw + alt_cal, 0))
+                        target_tracker.go(util.wrap_rad(unwrap(tracked_plane.az.value) + az_offset - azm_cal, scope_azm_raw - math.pi),
+                                          util.wrap_rad(unwrap(tracked_plane.el.value) + el_offset - alt_cal, scope_alt_raw - math.pi))
                     else:
-                        scope_ra_raw, scope_dec_raw = telescope.get_ra_dec()
-                        scope_ra = util.wrap_rad(scope_ra_raw + ra_cal, -math.pi)
-                        scope_dec = util.wrap_rad(scope_dec_raw + dec_cal, -math.pi)
-                        scope_alt, scope_azm = util.radec_to_altaz(scope_ra, scope_dec, observatory_location, util.get_current_time())
-                        scope_azm_alt = (scope_azm, scope_alt)
+                        tracked_plane_ra, tracked_plane_dec = util.altaz_to_radec(
+                            unwrap(tracked_plane.el.value) + el_offset,
+                            unwrap(tracked_plane.az.value) + az_offset,
+                            observatory_location,
+                            util.get_current_time())
+                        target_tracker.go(tracked_plane_ra - ra_cal, tracked_plane_dec - dec_cal)
+                else:
+                    # If no data is available for the target, stop tracking and inform the GUI of that fact.
+                    target_tracker.stop()
+                    gui_airplanes.stop_tracking()
+                    gui_offset_reader.reset_offsets()
 
-                    # Send new data to the to GUI so it can update the drawing.
-                    gui_sun_moon.update_positions(sun_azm_alt=unwrap(sun.az_el()),
-                                                  moon_azm_alt=unwrap(moon.az_el()))
-                    gui_telescope.update_telescope_location(scope_azm_alt)
-                    gui_airplanes.update_planes(planes)
+                # If we got to the end of the loop, communication is ok.
+                warn_comm_failure = False
+                unreliable_comm_count = 0
 
-                    # Receive new user inputs from the GUI.
-                    tracked_plane_hex_id = gui_airplanes.get_tracked_plane()
-                    az_offset, el_offset = gui_offset_reader.get_offsets()
-                    kp, ki, kd, gain_changes = gui_gain_reader.get_gains()
-
-                    # If the user requested a change to the controller gains, update them accordingly.
-                    if last_gain_changes != gain_changes:
-                        print('kp =', kp, 'ki =', ki, 'kd =', kd)
-                        target_tracker.set_gains(kp, ki, kd)
-                    last_gain_changes = gain_changes
-
-                    if util.azm_alt_ang_dist(scope_azm_alt, unwrap(sun.az_el())) < 20/180*math.pi:
-                        # We've strayed into the keep out circle around the Sun! Emergency Stop!
-                        # The user can fix this with the hand controller.
-                        target_tracker.stop()
-                        gui_airplanes.stop_tracking()
-                        gui_offset_reader.reset_offsets()
-                    elif tracked_plane_hex_id is None:
-                        # There is no airplane to track, so stop the tracker.
-                        target_tracker.stop()
-                    elif tracked_plane_hex_id in planes:
-                        # Extrapolate from the last known position and velocity of the plane to estimate the current position.
-                        tracked_plane = planes[tracked_plane_hex_id].extrapolate(time.monotonic_ns())
-
-                        # Inform the target tracker of the target position and the current position of the telescope.
-                        if args.mount_mode == 'altaz':
-                            target_tracker.go(util.wrap_rad(unwrap(tracked_plane.az.value) + az_offset - azm_cal, scope_azm_raw - math.pi),
-                                              util.wrap_rad(unwrap(tracked_plane.el.value) + el_offset - alt_cal, scope_alt_raw - math.pi))
-                        else:
-                            tracked_plane_ra, tracked_plane_dec = util.altaz_to_radec(
-                                unwrap(tracked_plane.el.value) + el_offset,
-                                unwrap(tracked_plane.az.value) + az_offset,
-                                observatory_location,
-                                util.get_current_time())
-                            target_tracker.go(tracked_plane_ra - ra_cal, tracked_plane_dec - dec_cal)
-                    else:
-                        # If no data is available for the target, stop tracking and inform the GUI of that fact.
-                        target_tracker.stop()
-                        gui_airplanes.stop_tracking()
-                        gui_offset_reader.reset_offsets()
-
-                    # If we got to the end of the loop, communication is ok.
-                    warn_comm_failure = False
-                    unreliable_comm_count = 0
-
-                except skywatcher.UnreliableCommError:
-                    unreliable_comm_count += 1
-                    if unreliable_comm_count > 5:
-                        print('Telescope communication lost! Attempting to continue...')
-                        warn_comm_failure = True
-                except nexstar.CommError:
-                    print(traceback.format_exc())
-                    print('Attempting to continue...')
+            except skywatcher.UnreliableCommError:
+                unreliable_comm_count += 1
+                if unreliable_comm_count > 5:
+                    print('Telescope communication lost! Attempting to continue...')
                     warn_comm_failure = True
-                    if args.telescope_protocol == 'skywatcher-mount-head-wifi':
-                        # This protocol can have inconsistent delays, wait a moment for
-                        # packets in flight to arrive before trying to resume communication.
-                        time.sleep(0.10)
-                except rpc.RpcConnectionFailure:
-                    print(traceback.format_exc())
-                    print('Attempting to continue...')
-                    warn_comm_failure = True
-                except rpc.RpcRemoteException:
-                    print(traceback.format_exc())
-                    print('Attempting to continue...')
-                    warn_comm_failure = True
-        except gui.Exit:
-            pass
-        finally:
-            # Stop the telescope and clean up.
-            telescope.slew_azmalt(0, 0)
-            sys.stdout.flush()
-            sys.stderr.flush()
-            serial_iface.close()
-            gui_iface.close()
-            sbs1_receiver.close()
-        return
+            except nexstar.CommError:
+                print(traceback.format_exc())
+                print('Attempting to continue...')
+                warn_comm_failure = True
+                if args.telescope_protocol == 'skywatcher-mount-head-wifi':
+                    # This protocol can have inconsistent delays, wait a moment for
+                    # packets in flight to arrive before trying to resume communication.
+                    time.sleep(0.10)
+            except rpc.RpcConnectionFailure:
+                print(traceback.format_exc())
+                print('Attempting to continue...')
+                warn_comm_failure = True
+            except rpc.RpcRemoteException:
+                print(traceback.format_exc())
+                print('Attempting to continue...')
+                warn_comm_failure = True
+    except gui.Exit:
+        pass
+    finally:
+        # Stop the telescope and clean up.
+        telescope.slew_azmalt(0, 0)
+        sys.stdout.flush()
+        sys.stderr.flush()
+        serial_iface.close()
+        gui_iface.close()
+        sbs1_receiver.close()
 
 if __name__ == '__main__':
     main()
